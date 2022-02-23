f3cb0ca992e2181e254fca34d2feb5ced17e9041
yifan <zhu.2278@osu.edu>
2022-02-23 00:21:20 -0500

    intergrating front and back:

--- backend/server.py
+++ backend/server.py
@@ -1,24 +1,30 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from wsgiref.handlers import SimpleHandler
+import requests
 
-# curl -X POST -H "Content-Type: text/plain" --data "this is raw data" http:localhost:8000
+# curl -X POST -H "Content-Type: text/plain" --data "this is raw data" http://localhost:8000
 
 
 class fooHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         self.send_response(200)
         self.send_header("content-type", 'text/plain')
+        self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
         content_len = int(self.headers.get('Content-Length'))
         post_body = self.rfile.read(content_len).decode("utf-8")
-        print(type(post_body))
-        print(post_body)
+        # print(type(post_body))
+        # print(post_body)
         res = self.foo(post_body)
+        # print(type(res))
+        # print(type(res.encode()))
         self.wfile.write(res.encode())
 
     def foo(self, data):
-        r = requests.get(url='http://google.com')
-        return r
+        print(data)
+        # r = requests.get(url='http://google.com')
+        # return r.text
+        return data.upper()
 
 
 def main():
@@ -29,5 +35,4 @@ def main():
 
 
 if __name__ == '__main__':
-    print("runned")
     main()
