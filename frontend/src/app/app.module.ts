42f6c380abf15040fa393032722b1e0d4a2517c2
yifan <zhu.2278@osu.edu>
2022-03-01 14:48:33 -0500

    create new view

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,9 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { AdvanceViewComponent } from './advance-view/advance-view.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdvanceViewComponent
   ],
   imports: [
     BrowserModule,
