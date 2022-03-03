d947416d4da9cdd8bfe1e484a9f8a6889f702df4
liao.489 <liaoyiqiao@hotmail.com>
2022-03-03 14:32:49 -0500

    Add ner person

--- ner/test_ner_person.py
+++ ner/test_ner_person.py
@@ -1,6 +1,11 @@
 import spacy
 
-nlp = spacy.load("en_core_web_sm")
+# nlp = spacy.load("en_core_web_sm")
+# # save model
+# nlp.to_disk('./ner/ner_person/')
+
+# load the saved model
+nlp = spacy.load('./ner/ner_person/')
 
 s = 'Our team members include Yifan Zhu, Tingyang Xie, Yuan Hong, Leon Cai, and Yiqiao Liao'
 doc = nlp(s)
