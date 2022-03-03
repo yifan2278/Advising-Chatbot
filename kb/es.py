b9c72dafe96f8a7393177d66c0a547b7fdcbebcf
Leon Cai <cai.855@buckeyemail.osu.edu>
2022-03-03 11:51:39 -0500

    improve es

--- kb/es.py
+++ kb/es.py
@@ -3,23 +3,81 @@ import time
 import os
 import json
 from elasticsearch import Elasticsearch
+import logging
 #set up path
 directory = 'C:/Users/123456/Source/Repos/Advising-Chatbot/data'
-res = requests.get('http://localhost:9200')
-print(res.content)
-es = Elasticsearch('http://localhost:9200')
-es.info()
-i = 1
-# load json file to elastic search
-for filename in os.listdir(directory):
-    if filename.endswith('.json'):
-        print(filename)
-        f = open(directory+'/'+filename)
-        docket_content = f.read()
-        es.index(index = 'myindex', ignore =400, id = i, body = json.loads(docket_content))
-        i = i+1
+
+
+
 
 # search in the KB
+def connect_elasticsearch():
+    _es = None
+    _es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+    if _es.ping():
+        print('Yay Connect')
+    else:
+        print('Awww it could not connect!')
+    return _es
+
+
+def create_index(es_object, index_name = 'classes'):
+    created = False
+    #index settings
+    settings = {
+        "settings": {
+            "number_of_shards": 1,
+            "number_of_replicas": 1
+        },
+        "mappings":{
+            'cse':{
+                'properties':{
+                    "num":{'type':'text'},
+                    'prereq':{'type':'text'},
+                    'track':{'type':'text'},
+                    'topic':{'type':'text'},
+                    'name':{'type':'text'},
+                    'desc':{'type':'text'},
+                    }}}}
+    try:
+        if not es_object.indices.exists(index_name):
+            # Ignore 400 means to ignore "Index Already Exist" error.
+            es_object.indices.create(index=index_name, ignore=400, body=settings)
+            print('Created Index')
+        created = True
+    except Exception as ex:
+        print(str(ex))
+    finally:
+        return created
+
+def search (es_object, index_name, search):
+    res = es_object.search(index = index_name, body = search)
+if __name__ == '__main__':
+    directory = 'C:/Users/123456/Source/Repos/Advising-Chatbot/data'
+    i = 1
+# load json file to elastic search
+
+    logging.basicConfig(level=logging.ERROR)
+    es = connect_elasticsearch()
+    init_index = create_index(es)
+    if not init_index:
+        print('Error: fail to create index')
+
+
+    for filename in os.listdir(directory):
+       if filename.endswith('course_data1.json'):
+             f = open(directory+'/'+filename)
+             docket_content = f.read()
+             es.index(index = 'classes', ignore =400, id = i, body = json.loads(docket_content))
+    i = i+1
+    if es is not None:
+        search_object = {
+            "query":{
+                "match_all":{}
+               } }
+        res =search(es, 'classes',search_object)
+        print(res)
+
 
 
 
