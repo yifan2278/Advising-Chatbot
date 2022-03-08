241927ea35a2cc9c8ebc26d82d079bc63fcc409a
liao.489 <liaoyiqiao@hotmail.com>
2022-03-08 15:35:40 -0500

    add data

--- util.py
+++ util.py
@@ -10,7 +10,7 @@ def get_class(clf, q):
     # if np.max(prob) > 0.2:
     #     res = clf.classes_[np.argmax(prob)]
     res = clf.classes_[np.argmax(prob)]
-    print('class:', res)
+    print('class:', res, np.max(prob))
     return res
 
 
@@ -37,35 +37,37 @@ if __name__ == '__main__':
     ner_class = spacy.load('./ner/ner_course/')
     ner_person = spacy.load('./ner/ner_person/')
 
-    q = input('Hello!\n').lower()
+    q = input('Hello!\n')
     q_class = get_class(clf, q)
-    while q_class != 'GOODBYE':
+    while True:#q_class != 'GOODBYE':
         if q_class == 'GREETING':
             print('Hi')
         elif q_class == 'PREREQ':
-            class_num = get_course_entity(ner_class, q)
+            class_num = get_course_entity(ner_class, q.lower())
             print('entity:', class_num)
         elif q_class == 'SIMILAR-COURSES':
-            class_num = get_course_entity(ner_class, q)
+            class_num = get_course_entity(ner_class, q.lower())
             print('entity:', class_num)
         elif q_class == 'RELATED-COURSES-AI':
             pass
+        elif q_class == 'RELATED-COURSES-PYTHON':
+            pass
         elif q_class == 'TOPICS':
-            class_num = get_course_entity(ner_class, q)
+            class_num = get_course_entity(ner_class, q.lower())
             print('entity:', class_num)
         elif q_class == 'WHO-TEACH':
-            class_num = get_course_entity(ner_class, q)
+            class_num = get_course_entity(ner_class, q.lower())
             print('entity:', class_num)
         elif q_class == 'TEACH-WHAT':
             person = get_person_entity(ner_person, q)
             print('entity:', person)
         elif q_class == 'AVAILABLE-SEC':
-            class_num = get_course_entity(ner_class, q)
+            class_num = get_course_entity(ner_class, q.lower())
             print('entity:', class_num)
         else:
             print('Currently not supported')
         print()
-        q = input('Hello again!\n').lower()
+        q = input('Hello again!\n')
         q_class = get_class(clf, q)
 
     print('Goodbye!')
