24a0757abc9ffd6789dfcb37f2b465bdad03f7d2
xie tingyang <33785682+spikexie@users.noreply.github.com>
2022-03-24 14:38:32 -0400

    Add files via upload

--- kb/elasticsearch_test.py
+++ kb/elasticsearch_test.py
@@ -7,8 +7,9 @@ headers = {
     'Content-Type': 'application/json'
 }
 
-file = open('course_data.json')
-def load_data(file):
+
+def load_data():
+    file = open('course_data.json')
     course_data = json.load(file)
     for i in range(len(course_data)):
         print(i)
@@ -22,25 +23,48 @@ def load_data(file):
     print('done')
 
 
-
-def search(entity, tag):
+def search(entity, tag=''):
+    entity = entity.upper()
+    tag = tag.lower()
     temp = requests.get(url='http://localhost:9200/course/_search',
                         data='{ "query": { "match" : { "num": { "query": "' + entity + '", "fuzziness": "2" } } } }',
                         headers=headers).json()
     hits1 = temp['hits']
-
     hits2 = hits1['hits']
-    info = hits2[0]['_source']
-    print(info[tag])
+    length = len(hits2)
+    # info = hits2[3]['_source']
+
+    # ele = info['hits']
+    if not len(entity) == 7:
+        for i in range(length):
+            info = hits2[i]['_source']
+            # print(info['num'])
+            if tag == '':
+                print('num: ' + info['num'])
+                print('name: ' + info['name'])
+                print('track: ' + info['track'])
+                print('prereq: ' + info['prereq'])
+                print('topic: ' + info['topic'])
+                print('desc:' + info['desc'])
+                print('\n')
+            else:
+                print(tag + ': ' + info[tag])
+    else:
+        info = hits2[0]['_source']
+        if tag == '':
+            print('num: ' + info['num'])
+            print('name: ' + info['name'])
+            print('track: ' + info['track'])
+            print('prereq: ' + info['prereq'])
+            print('topic: ' + info['topic'])
+            print('desc:' + info['desc'])
+            print('\n')
+        else:
+            print(tag + ': ' + info[tag])
+
 
 def deleteData():
     print(requests.delete(url='http://localhost:9200/_all').json())
 
 
-
-#test
-
-search('CSE2221', 'track')
-search('CSE5912', 'name')
-search('CSE5914', 'prereq')
-
+search('cse2221', 'prereq')
