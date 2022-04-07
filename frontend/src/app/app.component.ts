89dc5d060234a4b1b25f700c1cd6d31e2f98aac6
yifan <zhu.2278@osu.edu>
2022-04-07 13:48:01 -0400

    fix auto scroll

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, ViewChild, ElementRef, AfterViewInit, NgModule, SimpleChanges } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -24,6 +23,10 @@ export class AppComponent {
       console.log("Response from server " + data)
       this.msgs.push([data, 'Bot'])
     })
+    this.http.post("http://localhost:8000", this.inp, { responseType: 'text' }).subscribe(data => {
+      console.log("Response from server " + data)
+      this.msgs.push([data, 'Bot'])
+    })
     this.inp = ""
     let scrollInstance = this.scroll.nativeElement
     //auto scroll the chat window
@@ -33,17 +36,24 @@ export class AppComponent {
   }
 
   setView() {
+    let scrollInstance = this.scroll.nativeElement
+    let last = scrollInstance.children[scrollInstance.children.length - 1]
     console.log("ayay!!!")
     console.log(this.msgs)
-    let scrollInstance = this.scroll.nativeElement
-    let bot_msgs = scrollInstance.getElementsByClassName('bot')
-    let last = bot_msgs[bot_msgs.length - 1]
     console.log(this.msgs.length)
-    console.log(bot_msgs.length)
-    console.log(bot_msgs[1])
-    last.scrollIntoView({ behavior: 'smooth' })
+    console.log(scrollInstance.children)
+    console.log(scrollInstance.children.length)
+    console.log(last)
+    //last.scrollIntoView({ behavior: 'smooth' })
   }
 
-
+  ngAfterViewChecked() {
+    let len = this.scroll.nativeElement.children.length
+    console.log(len)
+    let last = this.scroll.nativeElement.children[len - 1]
+    if (len != 4) {
+      last.scrollIntoView({ behavior: 'smooth' })
+    }
+  }
 
 }
